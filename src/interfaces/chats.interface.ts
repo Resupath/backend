d7387a79e05170e852beb4bd7fc1dcc94d93f788
rimo <munak.dev@gmail.com>
2024-12-26 23:13:45 +0900

    feat: Room, Chat 인터페이스 정의

---
+++ src/interfaces/chats.interface.ts
@@ -0,0 +1,35 @@
+import { tags } from 'typia';
+import { User } from './user.interface';
+import { Character } from './characters.interface';
+import { Room } from './rooms.interface';
+
+export interface Chat {
+  id: string & tags.Format<'uuid'>;
+  roomId: Room['id'];
+  userId: User['id'] | null;
+  characterId: Character['id'] | null;
+  message: string & tags.MinLength<1>;
+  createdAt: string & tags.Format<'date-time'>;
+  deletedAt: string & tags.Format<'date-time'>;
+}
+
+export namespace Chat {
+  /**
+   * create
+   */
+  export interface CreateRequst
+    extends Pick<Chat, 'userId' | 'characterId' | 'message'> {}
+
+  export interface CreateResponse extends Pick<Chat, 'id' | 'message'> {}
+
+  /**
+   * get
+   */
+  export interface GetResponse
+    extends Pick<
+      Chat,
+      'id' | 'userId' | 'characterId' | 'message' | 'createdAt'
+    > {}
+
+  export interface GetAllResponse extends Array<Chat.GetResponse> {}
+}
