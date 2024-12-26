1bf771c674c9e93ac0864d612fe4f5ceafde97a2
rimo <munak.dev@gmail.com>
2024-12-26 23:15:01 +0900

    feat: 채팅 조회 API 추가
    - 조회할 채팅방의 아이디를 받아 모든 채팅을 조회한다.

--- src/controllers/rooms.controller.ts
+++ src/controllers/rooms.controller.ts
@@ -1,4 +1,17 @@
+import core from '@nestia/core';
 import { Controller } from '@nestjs/common';
+import { RoomsService } from 'src/services/rooms.service';
+import { tags } from 'typia';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+  constructor(private readonly roomsService: RoomsService) {}
+
+  /**
+   * 채팅방에 입력된 전체 채팅 기록을 조회한다.
+   */
+  @core.TypedRoute.Get('/:id')
+  async getAllChat(@core.TypedParam('id') id: string & tags.Format<'uuid'>) {
+    return await this.roomsService.getAll(id);
+  }
+}
--- src/services/rooms.service.ts
+++ src/services/rooms.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Chat } from 'src/interfaces/chats.interface';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getAll(roomId: string): Promise<Chat.GetAllResponse> {
+    const chats = await this.prisma.chat.findMany({
+      select: {
+        id: true,
+        user_id: true,
+        character_id: true,
+        message: true,
+        created_at: true,
+      },
+      where: { room_id: roomId },
+    });
+
+    /**
+     * mapping
+     */
+    return chats.map((el): Chat.GetResponse => {
+      return {
+        id: el.id,
+        userId: el.user_id,
+        characterId: el.character_id,
+        message: el.message,
+        createdAt: el.created_at.toISOString(),
+      };
+    });
+  }
+}
