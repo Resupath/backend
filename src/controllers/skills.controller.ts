6d4c7fabf49fb89fcfa01636ae8c1041aa5e6974
rimo <munak.dev@gmail.com>
2024-12-27 20:09:51 +0900

    feat: 스킬 생성, 조회 API 구현

--- src/controllers/skills.controller.ts
+++ src/controllers/skills.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import core from '@nestia/core';
+import { Controller, UseGuards } from '@nestjs/common';
+import { MemberGuard } from 'src/guards/member.guard';
+import { Skill } from 'src/interfaces/skills.interface';
 import { SkillsService } from 'src/services/skills.service';
 
 @Controller('skills')
 export class SkillsController {
   constructor(private readonly skillsService: SkillsService) {}
+
+  /**
+   * 스킬을 생성한다.
+   */
+  @UseGuards(MemberGuard)
+  @core.TypedRoute.Post()
+  async createSkill(
+    @core.TypedBody() body: Skill.CreateRequest,
+  ): Promise<Skill.CreateResponse> {
+    return await this.skillsService.create(body);
+  }
+
+  /**
+   * 스킬을 페이지네이션으로 조회한다.
+   */
+  @UseGuards(MemberGuard)
+  @core.TypedRoute.Get()
+  async getPositionByPage(
+    @core.TypedQuery() query: Skill.GetByPage,
+  ): Promise<Skill.GetByPageResponse> {
+    return await this.skillsService.getByPage(query);
+  }
 }
--- src/services/skills.service.ts
+++ src/services/skills.service.ts
@@ -1,4 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { Skill } from 'src/interfaces/skills.interface';
+import { DateTimeUtil } from 'src/util/dateTime.util';
+import { PrismaService } from './prisma.service';
+import { PaginationUtil } from 'src/util/pagination.util';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class SkillsService {}
+export class SkillsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(body: Skill.CreateRequest): Promise<Skill.CreateResponse> {
+    const date = DateTimeUtil.now();
+
+    return await this.prisma.skill.create({
+      select: { id: true },
+      data: { id: randomUUID(), keyword: body.keyword, created_at: date },
+    });
+  }
+
+  async getByPage(query: Skill.GetByPage): Promise<Skill.GetByPageResponse> {
+    const { skip, take } = PaginationUtil.getOffset(query);
+
+    const whereInput: Prisma.SkillWhereInput | undefined = query.search
+      ? { keyword: { contains: query.search } }
+      : undefined;
+
+    const [data, count] = await this.prisma.$transaction([
+      this.prisma.skill.findMany({
+        select: { id: true, keyword: true },
+        where: whereInput,
+        skip,
+        take,
+      }),
+
+      this.prisma.skill.count({ where: whereInput }),
+    ]);
+
+    return PaginationUtil.createResponse({
+      data,
+      count,
+      skip,
+      take,
+    });
+  }
+}
