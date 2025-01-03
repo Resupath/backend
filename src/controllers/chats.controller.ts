3c5c218f2c481f37880641352a2fbd31ae678d5d
rimo <munak.dev@gmail.com>
2025-01-03 23:19:46 +0900

    feat: 채팅 API에 UserGuard 추가

--- src/controllers/chats.controller.ts
+++ src/controllers/chats.controller.ts
@@ -1,7 +1,10 @@
 import core from '@nestia/core';
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from 'src/decorators/user.decorator';
+import { UserGuard } from 'src/guards/user.guard';
 import { Chat } from 'src/interfaces/chats.interface';
+import { Guard } from 'src/interfaces/guard.interface';
 import { Room } from 'src/interfaces/rooms.interface';
 import { ChatsService } from 'src/services/chats.service';
 
@@ -21,11 +24,13 @@ export class ChatsController {
   /**
    * 채팅방에 입력된 전체 채팅 기록을 바탕으로 캐릭터의 응답을 생성한다.
    */
+  @UseGuards(UserGuard)
   @core.TypedRoute.Post('/:roomId')
   async createChat(
+    @User() user: Guard.UserResponse,
     @core.TypedParam('roomId') roomId: Room['id'],
     @core.TypedBody() body: Chat.CreateRequst,
   ) {
-    return await this.chatsService.create(roomId, body);
+    return await this.chatsService.chat(user.id, roomId, body);
   }
 }
