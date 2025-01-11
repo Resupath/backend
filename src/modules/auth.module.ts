1b87c28739aab5f43288b0ed5fd3ee518e938b50
rimo <munak.dev@gmail.com>
2025-01-11 15:37:57 +0900

    refactor: Auth 모듈 글로벌로 변경

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers/auth.controller';
 import { AuthService } from 'src/services/auth.service';
 
+@Global()
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
