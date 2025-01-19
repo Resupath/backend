58a04500d4d1020c9723031e985b9c832605ea0c
rimo <munak.dev@gmail.com>
2025-01-19 14:07:08 +0900

    feat: Admin 캐릭터 조회 API 추가
    - 기존 캐릭터 리스트 조회 API를 이용한다.
    - 삭제된 캐릭터도 조회 가능.
    - optional 파라미터 오용의 위험이 있음으로 추후 더 좋은 구조로 개선.

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,13 +1,18 @@
 import core from '@nestia/core';
 import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Character } from 'src/interfaces/characters.interface';
 import { Personality } from 'src/interfaces/personalities.interface';
+import { CharactersService } from 'src/services/characters.service';
 import { PersonalitiesService } from 'src/services/personalities.service';
 
 @ApiTags('Admin')
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly personalitiesService: PersonalitiesService) {}
+  constructor(
+    private readonly personalitiesService: PersonalitiesService,
+    private readonly charactersService: CharactersService,
+  ) {}
 
   /**
    * 성격 데이터를 생성한다. 중복된 키워드는 한번만 저장된다.
@@ -15,5 +20,13 @@ export class AdminController {
   @core.TypedRoute.Post('personalities/bulk')
   async createPersonalities(@core.TypedBody() body: Personality.CreateBulkRequest): Promise<void> {
     return this.personalitiesService.createBulk(body);
+  }
+
+  /**
+   * 캐릭터를 조회한다. 사용자가 삭제한 캐릭터도 조회된다.
+   */
+  @core.TypedRoute.Get('characters')
+  async getCharacters(@core.TypedQuery() query: Character.GetByPageRequest): Promise<Character.GetByPageResponse> {
+    return await this.charactersService.getBypage(query, { deletedAt: true });
   }
 }
