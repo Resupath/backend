d3f6b5ae7d846fb14460f80543748cc217eea86b
rimo <munak.dev@gmail.com>
2025-03-02 00:00:54 +0900

    feat: pdf text 변환 로직 추가

--- src/modules/sources.module.ts
+++ src/modules/sources.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SourcesController } from 'src/controllers/sources.controller';
 import { NotionService } from 'src/services/notion.service';
+import { PdfService } from 'src/services/pdf.service';
 import { SourcesService } from '../services/sources.service';
 
 @Module({
   controllers: [SourcesController],
-  providers: [SourcesService, NotionService],
+  providers: [SourcesService, NotionService, PdfService],
   exports: [SourcesService, NotionService],
 })
 export class SourcesModule {}
