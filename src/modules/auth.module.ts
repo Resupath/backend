ff9e272bf9c9cb255ac3af9588771494985b5af9
rimo <munak.dev@gmail.com>
2025-03-04 22:43:55 +0900

    feat: OAuth 관련 인증로직 분리
    - OAuthService 새롭게 추가

--- src/modules/auth.module.ts
+++ src/modules/auth.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { AuthController } from 'src/controllers/auth.controller';
 import { AuthService } from 'src/services/auth.service';
+import { OAuthService } from 'src/services/oauth.service';
 
 @Global()
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, OAuthService],
   exports: [AuthService],
 })
 export class AuthModule {}
